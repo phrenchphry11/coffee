eebba8d24fddada3e925b0fe500c624327d8cb32
Holly French <holly.french@rd.io>
2015-07-22 07:27:26 -0700

    Initial Commit

---
+++ Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by Holly French on 7/22/15.
+//  Copyright (c) 2015 The Collection. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
